d514180f991dff9042744e25724f583d21eeee58
Tatsuki0810 <sss.53.88840@gmail.com>
2020-08-03 17:24:42 +0900

    Last Update

--- main.js
+++ main.js
@@ -1,20 +1,43 @@
 window.onload = function() {
     document.getElementById('send').onclick = function() {
-        post();
+        var element = document.getElementById("slc");
+        var a = element.value ;
+        if (a == "type1") {
+            var num = document.getElementById("num").value;
+            post(num, 0);
+        } else if (a == "type2") {
+            var num = document.getElementById("num").value;
+            post(num, 1);
+        }
     };
 
-    function post() {
-        var t1 = document.forms.mainform.type1;
-        var t2 = document.forms.mainform.type2;
-        var num = document.getElementById("num");
-        if (t1 == true) {
+    function post(v, t) {
+        console.log(v);
+        console.log(t);
+        if (t == 0) {
             console.log("Type1");
-            var n = num.toString(2);
+            var n = parseInt(v, 10).toString(2);
             document.getElementById("result").innerHTML = n;
-        } else {
+        } else if (t == 1) {
             console.log("Type2");
-            var n = parseInt(num, 2);
+            var n = parseInt(v, 2);
             document.getElementById("result").innerHTML = n;
+        }
+    }
+
+    document.getElementById("slc").onchange = function () {
+        var element = document.getElementById("slc") ;
+        var a = element.value ;
+        if (a == "type1") {
+            document.getElementById('send').onclick = function() {
+                var num = document.getElementById("num").value;
+                post(num, 0);
+            };
+        } else if (a == "type2") {
+            document.getElementById('send').onclick = function() {
+                var num = document.getElementById("num").value;
+                post(num, 1);
+            };
         }
     }
 }
